a73a7304c8167dc7ab43a0bfea5d3af0d94cd860
zaffinabrianna <zaffinabrianna@yahoo.com>
2025-07-03 22:08:14 -0700

    removed unnecessary files

--- server/db.js
+++
@@ -1,5 +0,0 @@
-/**
- * TODO:
- * - Set up PostgreSQL connection using pg or pg-promise
- * - Export the client or pool to use in routes.js
- */
--- server/server.js
+++
@@ -1,8 +0,0 @@
-/**
- * TODO:
- * - Set up Express server
- * - Serve static files from the 'public' folder
- * - Use middleware (body-parser or express.json())
- * - Mount routes from routes.js
- * - Start the server on a specified port
- */
